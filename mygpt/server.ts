72c866aba83ca837ea299c996d8099c18fb5759d
Shivamup51 <shivamupadhyay2868@gmail.com>
2025-06-18 14:13:33 +0530

    Refactor upload handling and update comments. Removed the upload.server route to streamline file management, and corrected a comment in server.ts for clarity.

--- mygpt/server.ts
+++ mygpt/server.ts
@@ -1,6 +1,6 @@
 import { createRequestHandler, type ServerBuild } from "@remix-run/cloudflare";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore This file won’t exist if it hasn’t yet been built
+// @ts-ignore This file won't exist if it hasn't yet been built
 import * as build from "./build/server"; // eslint-disable-line import/no-unresolved
 import { getLoadContext } from "./load-context";
 
